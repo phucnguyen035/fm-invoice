93698603aecbd2655b0e2d5b92087eccae16ab89
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2023-06-21 14:36:27 +0700

    feat: add get theme util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { ClassValue, clsx } from 'clsx'
+import { cookies } from 'next/headers'
+import { twMerge } from 'tailwind-merge'
+import { themeKey } from './constants'
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs))
+}
+
+export function getTheme(key = themeKey) {
+	return cookies().get(key)?.value ?? 'dark'
 }
