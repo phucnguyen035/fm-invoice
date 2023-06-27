bcb02c317bfbf6f2e4e7885f9daa3a944bd781a9
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2023-06-27 12:54:32 +0700

    feat: add get invoice id util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { ClassValue, clsx } from 'clsx'
 import { cookies } from 'next/headers'
 import { twMerge } from 'tailwind-merge'
+import { z } from 'zod'
 import { themeKey } from './constants'
 
 export function cn(...inputs: ClassValue[]) {
@@ -9,4 +10,15 @@ export function cn(...inputs: ClassValue[]) {
 
 export function getTheme(key = themeKey) {
 	return cookies().get(key)?.value ?? 'dark'
+}
+
+/**
+ * Get the first 6 characters of the last part of a UUUID v4
+ */
+export function getInvoiceId(id: string) {
+	if (!z.string().uuid().safeParse(id).success) {
+		throw new Error('Invoice ID must be a UUID!')
+	}
+
+	return id.split('-').pop()?.slice(0, 6)?.toUpperCase() ?? ''
 }
