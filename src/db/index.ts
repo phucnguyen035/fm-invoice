5f529131581cf0a435d9880d299c0b09a9551ff0
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2023-06-28 14:16:16 +0700

    feat: add find by status

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,21 +1,38 @@
-import { Pool } from '@neondatabase/serverless'
-import { drizzle } from 'drizzle-orm/neon-serverless'
+import { Pool as PoolNeon } from '@neondatabase/serverless'
+import { drizzle as drizzleNeon } from 'drizzle-orm/neon-serverless'
+import { drizzle as drizzleNode } from 'drizzle-orm/node-postgres'
+import { Pool as PoolNode } from 'pg'
 import { InvoiceMissingError } from '@/lib/error'
-import { invoices } from './schema'
+import { invoiceStatusEnum, invoices } from './schema'
 
 export type Database = {
 	invoices: typeof invoices
 }
 
-const pool = new Pool({ connectionString: process.env.PG_POOLED_URI })
+const db =
+	process.env.NODE_ENV === 'production'
+		? drizzleNeon<Database>(new PoolNeon({ connectionString: process.env.PG_POOLED_URI }), {
+				schema: { invoices },
+		  })
+		: drizzleNode<Database>(
+				new PoolNode({
+					connectionString: process.env.PG_POOLED_URI,
+				}),
+				{ logger: true, schema: { invoices } }
+		  )
 
-const db = drizzle<Database>(pool, {
-	logger: process.env.NODE_ENV !== 'production',
-	schema: { invoices },
-})
-
-export async function getInvoices(userId: string) {
-	return db.query.invoices.findMany({ where: ({ creator }, { eq }) => eq(creator, userId) })
+export async function getInvoices(userId: string, statuses: string[]) {
+	return db.query.invoices.findMany({
+		where: ({ status, creator }, { and, eq, or, sql }) =>
+			and(
+				eq(creator, userId),
+				statuses.length > 0
+					? or(
+							...statuses.map((s) => eq(status, sql`${s}::${sql.raw(invoiceStatusEnum.enumName)}`))
+					  )
+					: undefined
+			),
+	})
 }
 
 export async function getInvoiceFromId(invoiceId: string, userId: string) {
