7d5a2d4d87e588a61f0caeac9ccdda1da254b6f7
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2023-12-15 19:20:48 +0700

    chore: update seed invoices function

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -14,44 +14,37 @@ async function seed() {
 
 	await db.transaction(async (tx) => {
 		await tx.delete(invoicesTable)
-		await tx.insert(invoicesTable).values(createInvoices())
+		await tx.insert(invoicesTable).values(seedInvoices())
 	})
 }
 
-function createInvoices(length = 10) {
-	return Array.from(
-		{ length },
-		() =>
-			({
-				addressFrom: {
-					city: faker.location.city(),
-					country: faker.location.country(),
-					postCode: faker.location.zipCode(),
-					street: faker.location.streetAddress(),
-				},
-				addressTo: {
-					city: faker.location.city(),
-					country: faker.location.country(),
-					postCode: faker.location.zipCode(),
-					street: faker.location.streetAddress(),
-				},
-				clientEmail: faker.internet.email(),
-				clientName: faker.person.fullName(),
-				dueDate: faker.date.future(),
-				issueDate: faker.date.past(),
-				description: faker.commerce.productName(),
-				paymentTerms: faker.helpers.arrayElement(invoicePaymentTermsEnum.enumValues),
-				status: faker.helpers.arrayElement(invoiceStatusEnum.enumValues),
-				items: Array.from(
-					{ length: faker.helpers.rangeToNumber({ min: 2, max: length / 2 }) },
-					() => ({
-						name: faker.commerce.productName(),
-						price: faker.number.float({ min: 1, max: 1000, precision: 0.01 }),
-						quantity: parseInt(faker.finance.amount(1, 10)),
-					})
-				),
-			} satisfies NewInvoice)
-	)
+function seedInvoices(length = 10): NewInvoice[] {
+	return Array.from({ length }, () => ({
+		addressFrom: {
+			city: faker.location.city(),
+			country: faker.location.country(),
+			postCode: faker.location.zipCode(),
+			street: faker.location.streetAddress(),
+		},
+		addressTo: {
+			city: faker.location.city(),
+			country: faker.location.country(),
+			postCode: faker.location.zipCode(),
+			street: faker.location.streetAddress(),
+		},
+		clientEmail: faker.internet.email(),
+		clientName: faker.person.fullName(),
+		dueDate: faker.date.future(),
+		issueDate: faker.date.past(),
+		description: faker.commerce.productName(),
+		paymentTerms: faker.helpers.arrayElement(invoicePaymentTermsEnum.enumValues),
+		status: faker.helpers.arrayElement(invoiceStatusEnum.enumValues),
+		items: Array.from({ length: faker.helpers.rangeToNumber({ min: 2, max: length / 2 }) }, () => ({
+			name: faker.commerce.productName(),
+			price: faker.number.float({ min: 1, max: 1000, precision: 0.01 }),
+			quantity: parseInt(faker.finance.amount(1, 10)),
+		})),
+	}))
 }
 
 seed()
