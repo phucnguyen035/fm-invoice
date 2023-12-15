465d697a6a6ad8f29908d613812dbd790c0c6b01
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2023-12-15 19:16:43 +0700

    refactor: simplify schema definition

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,25 +1,20 @@
-import { InferModel } from 'drizzle-orm'
 import { jsonb, pgEnum, pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core'
-import { createInsertSchema } from 'drizzle-zod'
-import z from 'zod'
 
 export const invoiceStatusEnum = pgEnum('status', ['draft', 'pending', 'paid'])
 export const invoicePaymentTermsEnum = pgEnum('payment_terms', ['1', '7', '14', '30'])
 
-export const Address = z.object({
-	street: z.string(),
-	city: z.string(),
-	postCode: z.string(),
-	country: z.string(),
-})
-export type Address = z.infer<typeof Address>
+export type Address = {
+	street: string
+	city: string
+	postCode: string
+	country: string
+}
 
-export const Item = z.object({
-	name: z.string(),
-	quantity: z.number().nonnegative(),
-	price: z.number().nonnegative(),
-})
-export type Item = z.infer<typeof Item>
+export type Item = {
+	name: string
+	quantity: number
+	price: number
+}
 
 export const invoicesTable = pgTable('invoices', {
 	id: uuid('id').primaryKey().defaultRandom(),
@@ -36,12 +31,5 @@ export const invoicesTable = pgTable('invoices', {
 	clientEmail: text('client_email').notNull(),
 	paymentTerms: invoicePaymentTermsEnum('payment_terms').notNull(),
 })
-export type Invoice = InferModel<typeof invoicesTable>
-
-const insertInvoiceSchema = createInsertSchema(invoicesTable, {
-	addressFrom: Address,
-	addressTo: Address,
-	items: Item.array(),
-})
-
-export type NewInvoice = z.infer<typeof insertInvoiceSchema>
+export type Invoice = typeof invoicesTable.$inferSelect
+export type NewInvoice = typeof invoicesTable.$inferInsert
